8ba3b8d89cd627a56b1fd86ddea58b3b4c8520e3
Adonai Soares <adonaielite@gmail.com>
2022-04-05 20:52:30 -0300

    Jquery Filter plugin

--- src/js/core/includes.js
+++ src/js/core/includes.js
@@ -1,5 +1,13 @@
 import $ from 'jquery'
 
+const loadHtmlSucessCallbacks = []
+
+export function onLoadHtmlSuccess(callback){
+    if(!loadHtmlSucessCallbacks.includes(callback)){
+        loadHtmlSucessCallbacks.push(callback)
+    }
+}
+
 function loadIncludes(parent){
     if(!parent) parent = 'body'
     $(parent).find('[wm-include]').each((function(i,e){
@@ -9,7 +17,7 @@ function loadIncludes(parent){
             success(data){
                 $(e).html(data)
                 $(e).removeAttr('wm-include')
-
+                loadHtmlSucessCallbacks.forEach(callback => callback(data))
                 loadIncludes(e)
             }
         })
